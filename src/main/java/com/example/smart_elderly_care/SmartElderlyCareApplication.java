5f05572da82f69b0e1cfa1c9f2f5a59bacd57f3e
JoeSeoHee <tjgml0348@gmail.com>
2025-05-03 16:38:56 +0900

    Initial commit

---
+++ src/main/java/com/example/smart_elderly_care/SmartElderlyCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_elderly_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartElderlyCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartElderlyCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_elderly_care/SmartElderlyCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_elderly_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartElderlyCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
